94075f0a654f408b538999a72ace8fc7eaebfb12
sspark89 <sosn3@naver.com>
2025-02-17 11:42:01 +0900

    build 실행

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/buchet_portfolio/',
   plugins: [
     react(),
     tailwindcss(),
   ],
+  build: {
+    outDir: 'docs',
+  },
 })
