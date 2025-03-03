64dfb22c45b681780825111618f46b74b0d2eb06
sspark89 <sosn3@naver.com>
2025-03-03 22:29:43 +0900

    react-router-dom 처리

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import Main from './components/Main';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import EmptyPage from './components/EmptyPage';
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/buchet_portfolio" element={<Main/>} />
+          <Route path="*" element={<EmptyPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
