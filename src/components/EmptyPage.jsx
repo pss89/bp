b1a759421b992d22f95c769a0ebd1582a749fd60
sspark89 <sosn3@naver.com>
2025-03-16 00:50:44 +0900

    emptypage 처리

--- src/components/EmptyPage.jsx
+++ src/components/EmptyPage.jsx
@@ -3,8 +3,12 @@ import { Link } from 'react-router-dom';
 export default function EmptyPage() {
     return (
         <>
-            <h2>잘못된 접근입니다.</h2>
-            <Link to="/">돌아가기</Link>
+            <div className="flex items-center justify-center min-h-screen">
+            <div className="w-full text-center">
+                <h2>잘못된 접근입니다.</h2>
+                <Link to="/">돌아가기</Link>
+            </div>
+            </div>
         </>
     );
 }
